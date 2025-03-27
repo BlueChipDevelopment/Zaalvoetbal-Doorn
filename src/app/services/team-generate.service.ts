77bf9507309627033934c4e78df68b46be343db0
chris.kok <chris.dekok@visma.com>
2025-03-27 17:20:03 +0100

    chore: remove comments from team-generate.service

--- src/app/services/team-generate.service.ts
+++ src/app/services/team-generate.service.ts
@@ -11,7 +11,7 @@ export class TeamGenerateService {
   private players: Player[] = [];
   private goalKeepers: Player[] = [] as Player[];
   private defenders: Player[] = [] as Player[];
-  private midfielders: Player[] = [] as Player[];
+  private fieldPlayers: Player[] = [] as Player[];
   private strikers: Player[] = [] as Player[];
   private teams: Teams = {
     TeamA: { shirtcolor: 'white', squad: [], attack: 0, defense: 0, condition: 0, totalScore: 0 },
@@ -36,17 +36,12 @@ export class TeamGenerateService {
     // Distribute goalies first.
     this.goalKeepers = this.getPlayersByPosition(Positions.GOAL_KEEPER);
     if (this.goalKeepers.length > 0)
-      this.distributePlayersToTeamsByCoPilot(this.goalKeepers);
+      this.distributePlayersToTeams(this.goalKeepers);
 
-    // if (this.defenders.length > 0)
-    //   this.distributePlayersToTeamsByCoPilot(this.defenders);
-
-    this.midfielders = this.getPlayersByPosition(Positions.MIDFIELDER);
-    if (this.midfielders.length > 0)
-      this.distributePlayersToTeamsByCoPilot(this.midfielders);
-
-    // if (this.strikers.length > 0)
-    //   this.distributePlayersToTeamsByCoPilot(this.strikers);
+    // Distribute fieldplayers
+    this.fieldPlayers = this.getPlayersByPosition(Positions.MIDFIELDER);
+    if (this.fieldPlayers.length > 0)
+      this.distributePlayersToTeams(this.fieldPlayers);
 
     // Calculeer score
     const teamA: Player[] = this.teams['TeamA'].squad.map(name => this.players.find(player => player.name === name)!);
@@ -66,33 +61,6 @@ export class TeamGenerateService {
     return this.players.filter(player => player.position === position);
 }
 
-  // private calculatePlayerScore(player: Player): Player {
-  //   switch (player.position) {
-  //     case Positions.GOAL_KEEPER:
-  //       player.totalScore = player.rating;
-  //       break;
-  //     case Positions.DEFENDER:
-  //       player.totalScore =
-  //         player.defenceRating * 1 +
-  //         player.conditionRating * 0.7 +
-  //         player.attackRating * 0.5;
-  //       break;
-  //     case Positions.STRIKER:
-  //       player.totalScore =
-  //         player.defenceRating * 0.3 +
-  //         player.conditionRating * 0.6 +
-  //         player.attackRating * 1;
-  //       break;
-  //     case Positions.MIDFIELDER:
-  //       player.totalScore =
-  //         player.defenceRating * 0.6 +
-  //         player.conditionRating * 0.9 +
-  //         player.attackRating * 0.6;
-  //       break;
-  //   }
-  //   return player;
-  // }
-
   private sortByPositions(players: Player[]): void {
     players.forEach((player) => {
       switch (player.position) {
@@ -103,7 +71,7 @@ export class TeamGenerateService {
           this.defenders.push(player);
           break;
         case Positions.MIDFIELDER:
-          this.midfielders.push(player);
+          this.fieldPlayers.push(player);
           break;
         case Positions.STRIKER:
           this.strikers.push(player);
@@ -112,7 +80,7 @@ export class TeamGenerateService {
     });
   }
 
-  private distributePlayersToTeamsByCoPilot(players: Player[]): void {
+  private distributePlayersToTeams(players: Player[]): void {
     let teamA: Player[] = [];
     let teamB: Player[] = [];
 
@@ -154,93 +122,4 @@ export class TeamGenerateService {
   private getTeamRating(team: Player[]): number {
     return team.reduce((total, player) => total + player.rating, 0);
   }
-
-  // private distributePlayersToTeams(players: Player[]): void {
-  //   let team = ['TeamA' as keyof Teams, 'TeamB' as keyof Teams];
-  //   let chosenTeam = team[this.randomInt(0, 1)];
-  //   let oppositeTeam = this.switchTeam(chosenTeam);
-
-  //   if (
-  //     this.teams[chosenTeam].squad.length >=
-  //     this.teams[oppositeTeam].squad.length
-  //   )
-  //     chosenTeam = oppositeTeam;
-
-  //   let randomPlayerIndex = this.randomInt(0, players.length - 1);
-  //   players = this.addPlayerToTeam(chosenTeam, randomPlayerIndex, players);
-
-  //   let playersNotMutated = [...players];
-  //   for (let i = 0; i < playersNotMutated.length; i++) {
-  //     let counterPlayerIndex = this.getCounterPlayerIndex(chosenTeam, players);
-  //     chosenTeam = this.switchTeam(chosenTeam);
-  //     this.addPlayerToTeam(chosenTeam, counterPlayerIndex, players);
-  //   }
-  // }
-
-  // private randomInt(min: number, max: number): number {
-  //   //? min and max included
-  //   return Math.floor(Math.random() * (max - min + 1) + min);
-  // }
-
-  // private addPlayerToTeam(
-  //   team: keyof Teams,
-  //   playerIndex: number,
-  //   players: Player[]
-  // ): Player[] {
-  //   this.teams[team].squad.push(players[playerIndex].name);
-  //   this.teams[team].totalScore += players[playerIndex].totalScore;
-  //   players.splice(playerIndex, 1);
-  //   return players;
-  // }
-
-  // private switchTeam(team: keyof Teams): keyof Teams {
-  //   return team === 'TeamA'
-  //     ? ('TeamB' as keyof Teams)
-  //     : ('TeamA' as keyof Teams);
-  // }
-
-  // private getCounterPlayerIndex(team: keyof Teams, players: Player[]): number {
-  //   let currentTeamScore = this.teams[team].totalScore;
-  //   let counterTeamScore = this.teams[this.switchTeam(team)].totalScore;
-
-  //   if (counterTeamScore >= currentTeamScore)
-  //     return this.pickWorstPlayer(players);
-
-  //   let counterPlayerIndex = players.findIndex((player) => {
-  //     return currentTeamScore <= counterTeamScore + player.totalScore;
-  //   });
-
-  //   if (counterPlayerIndex == -1)
-  //     return this.pickBestPlayer(players);
-
-  //   return counterPlayerIndex;
-  // }
-
-  // private pickWorstPlayer(players: Player[]): number {
-  //   let worstScore = 0;
-  //   let worstPlayerIndex = 0;
-
-  //   players.forEach((player, index) => {
-  //     if (worstScore >= player.totalScore) {
-  //       worstScore = player.totalScore;
-  //       worstPlayerIndex = index;
-  //     }
-  //   });
-
-  //   return worstPlayerIndex;
-  // }
-
-  // private pickBestPlayer(players: Player[]): number {
-  //   let bestScore = 0;
-  //   let bestPlayerIndex = 0;
-
-  //   players.forEach((player, index) => {
-  //     if (bestScore <= player.totalScore) {
-  //       bestScore = player.totalScore;
-  //       bestPlayerIndex = index;
-  //     }
-  //   });
-
-  //   return bestPlayerIndex;
-  // }
 }
